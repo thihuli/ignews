5dd666c73611241c9ba74768b8ea3d91211c3aff
thihuli <thiagocandidof@hotmail.com>
2022-06-08 00:02:50 -0300

    feat: add next auth and authentication by GitHub

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,23 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from "next-auth/react"
+
 import styles from './styles.module.scss'
 
 export function SingInButton() {
-  const isUserLoggedIn = true;
-  const userName = 'Thiago Fernandes'
+  const { data: session, status } = useSession();
+
+  console.log('session', session)
 
   return (
     <button
       type="submit"
       className={styles.signInButton}
+      onClick={() => status === "authenticated" ?  signOut() :  signIn('github')}
     >
-      <FaGithub color={isUserLoggedIn ? '#04d361' : '#eba417'} />
-      {isUserLoggedIn ? userName : 'Sign in with Github'}
-      {isUserLoggedIn && <FiX color='#737380' className={styles.closeIcon}/> }
+      <FaGithub color={status === "authenticated" ? '#04d361' : '#eba417'} />
+      {status === "authenticated" ? session.user.name : 'Sign in with Github'}
+      {status === "authenticated" && <FiX color='#737380' className={styles.closeIcon}/> }
     </button>
   )
 }
